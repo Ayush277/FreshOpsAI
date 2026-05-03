80392e66107a75ef5514b42dc327cf74c67c15f7
Ayush277 <prince908ayush@gmail.com>
2026-05-03 14:08:48 +0530

    d

--- frontend/src/pages/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage.jsx
@@ -4,7 +4,7 @@ import { AppShell } from '../components/layout/AppShell';
 export const DashboardPage = () => {
   return (
     <AppShell
-      title="DEVOPS"
+      title="DEVOPS changes"
       subtitle=""
     >
       <ExecutiveSummary />
