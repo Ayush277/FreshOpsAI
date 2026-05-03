793c189345c656c8863e13b5a178572fbb9aef2e
Ayush277 <prince908ayush@gmail.com>
2026-05-03 14:32:19 +0530

    cicd chnages

--- frontend/src/pages/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage.jsx
@@ -4,7 +4,7 @@ import { AppShell } from '../components/layout/AppShell';
 export const DashboardPage = () => {
   return (
     <AppShell
-      title="DEVOPS changes"
+      title="DEVOPS BATCH 2 -AYUSH"
       subtitle=""
     >
       <ExecutiveSummary />
