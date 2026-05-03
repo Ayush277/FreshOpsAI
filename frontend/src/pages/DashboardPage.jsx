a1aa8a38ce34be18b85c89eb2cbb5ed78fe121e3
Ayush277 <prince908ayush@gmail.com>
2026-05-03 14:06:11 +0530

    .chnages

--- frontend/src/pages/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage.jsx
@@ -4,7 +4,7 @@ import { AppShell } from '../components/layout/AppShell';
 export const DashboardPage = () => {
   return (
     <AppShell
-      title="DEVOPS PROJECT DASHBOARD"
+      title="DEVOPS DASHBOARD"
       subtitle=""
     >
       <ExecutiveSummary />
